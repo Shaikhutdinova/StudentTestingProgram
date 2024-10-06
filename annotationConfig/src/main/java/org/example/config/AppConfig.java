9fae037b7b59442456de98ebe3991f1e469db2ad
Natalia Shaikhutdinova <nat12031417@gmail.com>
2024-10-06 20:03:18 +0300

    add application.properties,

--- annotationConfig/src/main/java/org/example/config/AppConfig.java
+++ annotationConfig/src/main/java/org/example/config/AppConfig.java
@@ -1,14 +1,22 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+@PropertySource("classpath:application.properties")
 @Configuration
 public class AppConfig {
+
+    @Value("${questions.csv.path}")
+    private String questionsCsvPath;
+
     @Bean
     public Resource questionsResource() {
-        return new ClassPathResource("questions.csv");
+//        return new ClassPathResource("questions.csv");
+        return new ClassPathResource(questionsCsvPath.substring("classpath:".length()));
     }
 }
